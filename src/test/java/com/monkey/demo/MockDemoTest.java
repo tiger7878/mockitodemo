3664613f2c49ab7656a278205b99fdbf4035ebb3
tiger7878 <zhcw63@163.com>
2018-04-23 10:03:27 +0800

    添加MockDemoTest.java文件

--- src/test/java/com/monkey/demo/MockDemoTest.java
+++ src/test/java/com/monkey/demo/MockDemoTest.java
@@ -1,9 +1,29 @@
 package com.monkey.demo;
 
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
 /**
  * mock示例
  * User: monkey
  * Date: 2018-04-23 9:52
  */
 public class MockDemoTest {
+
+    //验证行为
+    @Test
+    public void verify_behaviour(){
+        //模拟创建一个List对象
+        List list=mock(List.class);
+        //使用mock的对象
+        list.add(1);
+        list.clear();
+        //验证add(1)和clear()行为是否发生
+        verify(list).add(1);
+        verify(list).clear();
+    }
 }
