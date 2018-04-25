59cb8c4fd7882d90f085308349b4a654a713d79a
tiger7878 <zhcw63@163.com>
2018-04-25 16:31:32 +0800

    新增加两个java类文件

---
+++ src/main/java/com/monkey/demo/Account.java
@@ -0,0 +1,17 @@
+package com.monkey.demo;
+
+/**
+ * User: monkey
+ * Date: 2018-04-23 11:51
+ */
+public class Account {
+    private RailwayTicket railwayTicket;
+
+    public RailwayTicket getRailwayTicket() {
+        return railwayTicket;
+    }
+
+    public void setRailwayTicket(RailwayTicket railwayTicket) {
+        this.railwayTicket = railwayTicket;
+    }
+}
---
+++ src/main/java/com/monkey/demo/RailwayTicket.java
@@ -0,0 +1,17 @@
+package com.monkey.demo;
+
+/**
+ * User: monkey
+ * Date: 2018-04-23 11:50
+ */
+public class RailwayTicket {
+    private String destination;
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+}
